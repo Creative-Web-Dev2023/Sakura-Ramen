eecc064f958396277e8b02026374d5025eadcd16
Gabriela Ströbl <162339336+Nini76gaby@users.noreply.github.com>
2025-06-12 09:24:22 +0200

    sakura Ordner als normales Verzeichnis eingebunden

---
+++ sakura/src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,17 @@
+import{Component}from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, NavbarComponent],
+  template: `<section>
+    <app-navbar></app-navbar>
+    <h1 class="fontRaleway">Sakura Ramen</h1>
+    <h2>Best Ramen in Town</h2>
+  </section>`,
+  styleUrls:['./landingPage.component.scss']
+})
+export class LandingPageComponent{}
